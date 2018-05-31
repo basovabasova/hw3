1326024ab486436adb2c9981ac11017da2912d1c
Olga <fedosova.olya@gmail.com>
2018-05-31 22:42:25 +0300

    исправила

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+    $continents = [
+        'Australia' => ['Sarcophilus harrisii', 'Pteropus vampyrus', 'Macropus giganteus'],
+        'Antarctica' => ['Euphausia superba', 'Aptenodytes forsteri'],
+        'North America' => ['Sciurus vulgaris', 'Felis silvestris catus'],
+        'South America' => ['Nesophontes edithae', 'Eleutherodactylus coqui'],
+        'Africa' => ['Crocodylus niloticus', 'Potamochoerus porcus', 'Suncus etruscus']
+    ];
+
+    $continent = [];
+    $first = [];
+    $second = [];  
+    $new = [];
+    $crazy = [];
+    $crazy_world =[];
+    $arr_animals = [];
+    
+    foreach ($continents as $key => $world) {
+        $continent[] = $key;
+        foreach ($world as $key => $animals) {
+            $a = str_word_count($animals);
+            if ($a === 2) {            
+                $animal = explode(' ', $animals);
+                $new[] = $animal;                  
+                $first[] = $animal[0];
+                $second[] = $animal[1];
+            }
+        }
+    }
+
+    shuffle($second);
+
+    for ($i=0; $i < count($new) ; $i++) { 
+        $crazy[] = $first[$i] . ' ' . $second[$i];
+    }
+
+    foreach ($crazy as $value) {
+        $first_word = explode(' ', $value);
+        foreach ($continents as $key => $animals) {
+            foreach ($animals as $animal) {
+                $first_word_1 = explode(' ', $animal);
+                if ($first_word[0] === $first_word_1[0]) {
+                    $crazy_world[$key][] = $value;
+                }
+            }
+        }
+    }
+
+    foreach ($crazy_world as $key => $animals) {
+        $arr_animals[$key] = $animals;
+    }
+
+    foreach ($arr_animals as $key => $animals) {
+        echo "<h2>$key</h2>";
+        echo implode(', ', $animals);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>hw3</title>
+</head>
+<body>
+</body>
+</html>
--- lesson.php
+++ lesson.php
@@ -8,32 +8,24 @@ something text
 something text
 something text
 TEXT;
-
+echo '<pre>';
 var_dump($a);
-echo "<br>";
 var_dump($b);
-echo "<br>";
 var_dump($c);
-echo "<br>";
 var_dump($d);
 
 $e = 38 + "1";
-echo "<br>";
 var_dump($e);
 
 $f = 'very good string';
 $g = substr($f, 2, 5);
-echo "<br>";
 var_dump($f);
-echo "<br>";
 var_dump($g);
 
 $h = str_replace('good', 'bad', $f);
-echo "<br>";
 var_dump($h);
 
 $i = strpos($h, 'bad');
-echo "<br>";
 var_dump($i);
 if ($i === false) {
     echo "bad' не найден в строке '$h'";
@@ -46,54 +38,37 @@ var_dump($j);
 
 $k = '<p>Параграф.</p><!-- Комментарий --> <a href="#fragment">Tекст</a>';
 $l = strip_tags($k);
-echo "<br>";
 var_dump($l);
 $m = strip_tags($k, '<p><a>');
-echo "<br>";
 var_dump($m);
-echo "<br>";
 
 $n = 'apple';
 $o = md5($n);
 var_dump($n);
-echo "<br>";
 var_dump($o);
 
 // массивы
 $p = [10, 11, 3, 14, 8];
-echo "<br>";
 var_dump($p);
-echo "<br>";
 echo $p;
-echo "<br>";
 print_r($p);
 
 $r = [100 => 2, 200 => 4];
-echo "<br>";
 var_dump($r);
-echo "<br>";
 echo $r;
-echo "<br>";
 print_r($r[200]);
 
 $s = ['name' => 'Victor', 'city' => 'SPb'];
-echo "<br>";
 var_dump($s);
-echo "<br>";
 echo $s;
-echo "<br>";
 print_r($s[name]);
 
 $eggs[] = 'новое яйцо';
-echo "<br>";
 print_r($eggs);
 $eggs[15] = 'уейн грецки';
-echo "<br>";
 print_r($eggs);
 unset($eggs[15]);
-echo "<br>";
 print_r($eggs);
-echo "<br>";
 
 for ($i=0; $i<count($eggs); $i++)
 {
@@ -113,7 +88,6 @@ $t = [];
 foreach ($s as $value) {
   $t[] = $value; //"перепаковываем" ключи по порядку
 }
-echo "<br>";
 print_r($t);
 
 $cities = [
@@ -121,7 +95,6 @@ $cities = [
     "Германия" => ["Берлин", "Франкфурт", "Мюнхен"],
     "Франция" => ["Париж", "Марсель"]
 ];
-echo "<br>";
 echo $cities['Россия'][2] . '<br>'; 
 
 foreach ($cities as $key => $value) {
@@ -131,7 +104,7 @@ foreach ($cities as $key => $value) {
     echo "$city <br>";
   }
 }
-echo "<br>";
+
 $cities1 = [
     ["Москва", "Санкт-Петербург", "Нижний Новгород"],
     ["Берлин", "Франкфурт", "Мюнхен"],
@@ -147,44 +120,47 @@ for ($i=0; $i < count($cities1); $i++) {
 }
 
 $u = explode(' ', $h);
-echo "<br>";
 echo $u[1];
 
 $data = "foo:*:1023:1000::/home/foo:/bin/sh";
 list($user, $pass, $uid, $gid, $gecos, $home, $shell) = explode(":", $data);
-echo "<br>";
 echo $user; 
-echo "<br>";
 echo $pass;
-echo "<br>";
 echo $gid;
-echo "<br>";
 print_r(explode(':', $data, 3));
-echo "<br>";
 print_r(explode(':', $data, -2));
-echo "<br>";
 print_r(explode(' ', $data, -2));//Если delimiter не содержится в string, и используется отрицательный limit, то будет возвращен пустой массив (array)
 
 //print_r(explode('', $data)); Если delimiter является пустой строкой (""), explode() возвращает FALSE.
 
-echo "<br>";
 var_dump(implode(',', $p));
-echo "<br>";
 var_dump(implode(',', array()));
-echo "<br>";
 echo is_array($s) ? 'Массив' : 'Не массив';
-echo "<br>";
 if (in_array("Victor", $s)) {
     echo "Нашел!"; //регистрозависима
 }
-echo "<br>";
 print_r(array_keys($s));
-echo "<br>";
 print_r(array_shift($s));
-echo "<br>";
 print_r($s);
-echo "<br>";
 print_r(array_search('4', $r));
+
+//cписки
+
+$row = ['Victor', 'SPb'];
+list($name, $city) = $row;
+var_dump($name);
+var_dump($city);
+
+//ресурсы
+
+$v = fopen('animals.php', 'r');
+var_dump($v);
+
+$w = fread($v, 1024);
+print_r($w);
+
+//объекты
+
 
 ?>
 
